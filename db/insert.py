b16fdfde1398b21390f6f06a189133da6cdca45f
Edward Cooper <edcooper.dev@gmail.com>
2025-06-19 15:47:16 -0500

    added logic to insert data into postgres

---
+++ db/insert.py
@@ -0,0 +1,30 @@
+import psycopg2
+from config import DB_CONFIG
+
+def connect():
+    return psycopg2.connect(**DB_CONFIG)
+
+def insert_game(conn, game):
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO games (game_date, home_team, away_team)
+            VALUES (%s, %s, %s)
+            RETURNING id
+        """, (game["game_date"], game["home_team"], game["away_team"]))
+        return cur.fetchone()[0]
+
+def insert_odds(conn, game_id, game, sportsbook="FanDuel"):
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO odds (
+                game_id, sportsbook, moneyline_home, moneyline_away,
+                spread_home, spread_away, spread_odds_home, spread_odds_away,
+                total, over_odds, under_odds
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            game_id, sportsbook,
+            game["moneyline_home"], game["moneyline_away"],
+            game["spread_home"], game["spread_away"],
+            game["spread_odds_home"], game["spread_odds_away"],
+            game["total"], game["over_odds"], game["under_odds"]
+        ))
